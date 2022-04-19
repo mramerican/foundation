f63ae8430c5b0c7c75257b7abe0adf71487d62d0
Andrii Halich <mramerican@i.ua>
2022-04-19 11:28:09 +0300

    add googletagmanager

--- wp-content/themes/theme-sp/header.php
+++ wp-content/themes/theme-sp/header.php
@@ -154,6 +154,15 @@ $langAttr = $langAttr === 'lang="ru-RU"'? 'lang="ru"':  $langAttr
   <?php endif; ?>
 
   <link rel="dns-prefetch" href="https://www.cloudflare.com">
+
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push(
+      {'gtm.start': new Date().getTime(),event:'gtm.js'}
+    );var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MWLW8GN');</script>
+  <!-- End Google Tag Manager -->
 </head>
 <body <?php body_class(); ?> style="overflow:hidden;" id="_body">
 <div class="bodyWr">
