81f855e2d8864e487dd90a609145540e4a0c49b0
Andrii Halich <mramerican@i.ua>
2022-07-08 13:04:35 +0300

    delete updraft theme

--- wp-content/updraft/themes-old/theme-sp/inc/lang.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * register string for translate
- */
-add_action('init', function() {
-//  pll_register_string('theme-sp', 'Показати більше');
-  pll_register_string('theme-sp', 'Приховати');
-});
